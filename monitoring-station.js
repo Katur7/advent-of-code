35a2878192cbf5e47d65458b38fa737084176b03
Grímur Kristinsson <grimurk@gmail.com>
2019-12-15 21:35:44 +0000

    Monitoring station - part 2

--- monitoring-station.js
+++ monitoring-station.js
@@ -8,26 +8,111 @@ function readMap() {
 }
 
 const map = readMap();
-const best = {
-  point: {x: 0, y: 0},
-  count: -1
-};
-for(const p of mapIterator(map)) {
-  if(isAsteroid(p, map)) {
-    const asteroids = countAsteroids(p, map);
-    if(asteroids > best.count) {
-      best.point = p;
-      best.count = asteroids;
+partOne(map);
+partTwo(map);
+
+function partOne(map) {
+  const best = {
+    point: {x: 0, y: 0},
+    count: -1
+  };
+  for(const p of mapIterator(map)) {
+    if(isAsteroid(p, map)) {
+      const asteroids = countAsteroids(p, map);
+      if(asteroids > best.count) {
+        best.point = p;
+        best.count = asteroids;
+      }
     }
   }
+  console.log(best);
 }
-console.log(best);
-// console.log(countAsteroids({x: 1, y: 2}, map));
+
+function partTwo(map) {
+  const station = {x: 23, y: 29};
+  let kills = 0;
+  const { topRight, bottomRight, bottomLeft, topLeft } = asteroidsByQuadrants(station, map);
+  kills = killQuadrant(kills, topRight);
+  kills = killQuadrant(kills, bottomRight);
+  kills = killQuadrant(kills, bottomLeft);
+  kills = killQuadrant(kills, topLeft);
+}
+
+function killQuadrant(kills, quadrant) {
+  const keys = Array.from(quadrant.keys()).sort((a, b) => a - b);
+  for(const slope of keys) {
+    const asteroids = quadrant.get(slope);
+    kills++;
+    if(kills === 200) {
+      console.log(kills, asteroids[0]);
+    }
+    if(asteroids.length > 1) {
+      quadrant.set(slope, asteroids.slice(1));
+    } else {
+      quadrant.delete(slope);
+    }
+  }
+  return kills;
+}
+
+function asteroidsByQuadrants(station, map) {
+  const sMap = slopeMap(station, map);
+
+  const height = map.length - 1;
+  const width = map[0].length - 1;
+  const topRight = new Map();
+  const bottomRight = new Map();
+  const bottomLeft = new Map();
+  const topLeft = new Map();
+
+  for(let y = 0; y < station.y; y++) {
+    for(let x = station.x; x <= width; x++) {
+      const slope = sMap[y][x];
+      if(slope !== null && !topRight.has(slope)) {
+        const asteroidsOnSlope = asteroidsInALine(station, {x, y}, height, width);
+        topRight.set(slope, asteroidsOnSlope);
+      }
+    }
+  }
+  for(let y = station.y; y <= height; y++) {
+    for(let x = station.x; x <= width; x++) {
+      const slope = sMap[y][x];
+      if(slope !== null && !bottomRight.has(slope)) {
+        const asteroidsOnSlope = asteroidsInALine(station, {x, y}, height, width);
+        bottomRight.set(slope, asteroidsOnSlope);
+      }
+    }
+  }
+  for(let y = station.y; y <= height; y++) {
+    for(let x = 0; x < station.x; x++) {
+      const slope = sMap[y][x];
+      if(slope !== null && !bottomLeft.has(slope)) {
+        const asteroidsOnSlope = asteroidsInALine(station, {x, y}, height, width);
+        bottomLeft.set(slope, asteroidsOnSlope);
+      }
+    }
+  }
+  for(let y = 0; y < station.y; y++) {
+    for(let x = 0; x < station.x; x++) {
+      const slope = sMap[y][x];
+      if(slope !== null && !topLeft.has(slope)) {
+        const asteroidsOnSlope = asteroidsInALine(station, {x, y}, height, width);
+        topLeft.set(slope, asteroidsOnSlope);
+      }
+    }
+  }
+  return {
+    topRight,
+    bottomRight,
+    bottomLeft,
+    topLeft
+  }
+}
 
 function countAsteroids(potentialStation, map) {
   const asteroids = new Set();
   for(const b of mapIterator(map)) {
-    const points = pointsInALine(potentialStation, b);
+    const points = pointsInALine(potentialStation, b, map.length - 1, map[0].length - 1);
     const asteroid = points.find(p => isAsteroid(p, map));
     if(asteroid) {
       asteroids.add(asteroid.x + ',' + asteroid.y);
@@ -36,35 +121,40 @@ function countAsteroids(potentialStation, map) {
   return asteroids.size;
 }
 
-function pointsInALine(p1, p2) {
+function asteroidsInALine(p1, p2, height, width) {
+  const pointsOnSlope = pointsInALine(p1, p2, height, width);
+  return pointsOnSlope.filter(p => isAsteroid(p, map));
+}
+
+function pointsInALine(p1, p2, height, width) {
   if(isSamePoint(p1, p2)) {
     return [];
   }
 
-  const slope = (p2.y - p1.y) / (p2.x - p1.x);
+  const slope = calcSlope(p1, p2);
   const c = p1.y - p1.x * slope;
   const points = [];
   if(Math.abs(slope) === Infinity) {
     // Vertical line
     if(p1.y < p2.y) {
       // Downwards slope
-      for(let y = p1.y + 1; y <= p2.y; y++) {
+      for(let y = p1.y + 1; y <= height; y++) {
         points.push({x: p1.x, y});
       }
     } else {
       // Upwards slope
-      for(let y = p1.y - 1; y >= p2.y; y--) {
+      for(let y = p1.y - 1; y >= 0; y--) {
         points.push({x: p1.x, y});
       }
     }
     return points;
   }
   
-  const minY = Math.min(p1.y, p2.y);
-  const maxY = Math.max(p1.y, p2.y);
+  const minY = Math.min(p1.y, 0);
+  const maxY = Math.max(p1.y, height);
   if(p1.x < p2.x) {
     // Righwards slope
-    for(let x = p1.x + 1; x <= p2.x; x++) {
+    for(let x = p1.x + 1; x <= width; x++) {
       const y = calcY(slope, x, c);
       if(Number.isInteger(y) && y >=  minY && y <= maxY) {
         points.push({x, y});
@@ -72,7 +162,7 @@ function pointsInALine(p1, p2) {
     }
   } else {
     // Leftwards slope
-    for(let x = p1.x - 1; x >= p2.x; x--) {
+    for(let x = p1.x - 1; x >= 0; x--) {
       const y = calcY(slope, x, c);
       if(Number.isInteger(y) && y >=  minY && y <= maxY) {
         points.push({x, y});
@@ -87,6 +177,22 @@ function pointsInALine(p1, p2) {
   }
 }
 
+function calcSlope(p1, p2) {
+  return (p2.y - p1.y) / (p2.x - p1.x);
+}
+
+function slopeMap(station, map) {
+  const slopeMap = deepCopyMap(map);
+  for(const p of mapIterator(slopeMap)) {
+    if(isAsteroid(p, slopeMap) && !isSamePoint(station, p)) {
+      slopeMap[p.y][p.x] = calcSlope(station, p);
+    } else {
+      slopeMap[p.y][p.x] = null;
+    }
+  }
+  return slopeMap;
+}
+
 function* mapIterator(map) {
   for(let y = 0; y < map.length; y++) {
     for(let x = 0; x < map[0].length; x++) {
@@ -95,67 +201,19 @@ function* mapIterator(map) {
   }
 }
 
-function* mapBoundaryIterator(point, map) {
-  const height = map.length;
-  const width = map[0].length;
-  const topLeft = {x: 0, y: 0};
-  const topRight = {x: width - 1, y: 0};
-  const bottomLeft = {x: 0, y: height - 1};
-  const bottomRight = {x: width - 1, y: height - 1};
-
-  if(point.y === 0 ) {
-    if(point.x === 0) {
-      yield topRight;
-    } else if (point.x === width - 1) {
-      yield topLeft;
-    } else {
-      yield topLeft;
-      yield topRight;
-    }
-  } else {
-    for(let x = 0; x < width; x++) {
-      yield {x, y: 0};
-    }
-  }
-
-  if(point.x === width - 1) {
-    if(!isSamePoint(point, bottomRight)) {
-      yield bottomRight;
-    }
-  } else {
-    for(let y = 1; y < height; y++) {
-      yield {x: width - 1, y};
-    }
-  }
-
-  if(point.y === height - 1) {
-    if(!isSamePoint(point, bottomLeft)) {
-      yield bottomLeft;
-    }
-  } else {
-    for(let x = width - 2; x >= 0; x--) {
-      yield {x, y: height - 1};
-    }
-  }
-
-  if(point.x !== 0) {
-    for(let y = height - 2; y > 0; y--) {
-      yield {x: 0, y};
-    }
-  }
-
-}
-
 function isSamePoint(p1, p2) {
   return p1.x === p2.x && p1.y === p2.y;
 }
 
 function isAsteroid(point, map) {
-  return map[point.y][point.x] === '#';
+  try {
+    return map[point.y][point.x] === '#';
+  } catch (e) {
+    console.error(point, map);
+    throw e;
+  }
 }
 
-function calcDistance(p1, p2) {
-  const x = Math.abs(p1.x - p2.x);
-  const y = Math.abs(p1.y - p2.y);
-  return x + y;
+function deepCopyMap(map) {
+  return map.map(line => line.slice());
 }
